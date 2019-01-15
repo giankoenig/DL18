a4f382350e208311482a2ff920150aecacb8b241
Samuel <samuel@localhost.localdomain>
2019-01-15 16:27:26 +0100

    added saving to eval_policy

--- dl_mix/eval_policy.py
+++ dl_mix/eval_policy.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import shutil
 from hyperopt import hp, fmin, tpe, Trials, STATUS_OK
 from search_space import SearchSpace
+import pickle
 
 tf.flags.DEFINE_string('model_name', 'wrn',
                        'wrn, shake_shake_32, shake_shake_96, shake_shake_112, '
@@ -41,24 +42,42 @@ def eval_wrn_40_2(args):
   shutil.rmtree(FLAGS.checkpoint_dir)
   return 1-result[2]
 
-def eval_wrn(args):
-    ops = args['sub_policy'].split('_')
-    policy = [(ops[0], args['Prob2'], args['Mag1']),(ops[1], args['Prob2'], args['Mag2'])]
-    print(policy)
-    return 0
+def eval_fake(args):
+  ops = args['sub_policy'].split('_')
+  policy = [(ops[0], args['Prob2'], args['Mag1']),(ops[1], args['Prob2'], args['Mag2'])]
+  print(policy)
+  return 0
+
+def run_trial():
+  
+  trials_step = 1  # how many additional trials to do after loading saved trials. 1 = save after iteration
+  max_trials = 0  # initial max_trials. put something small to not have to wait
+  model_name = 'eval_trials_wrn_40_2.hyperopt'
+  
+  try:  # try to load an already saved trials object, and increase the max
+	trials = pickle.load(open(model_name, "rb"))
+	print("Found saved Trials! Loading...")
+	max_trials = len(trials.trials) + trials_step
+	print("Rerunning from {} trials to {} (+{}) trials".format(len(trials.trials), max_trials, trials_step))
+  except:  # create a new trials object and start searching
+	trials = Trials()
+  best = fmin(eval_wrn_40_2,
+    	space=SearchSpace(),    
+    	algo=tpe.suggest,
+    	max_evals=max_trials,
+    	trials=trials)
+
+  print('Best: ', best)
+
+  # save the trials object
+  with open(model_name, "wb") as f:
+    pickle.dump(trials, f)
 
 def main(_):
   print('starting training')
   
-  trials = Trials()
-
-  best = fmin(eval_wrn_40_2,
-    	space=SearchSpace(),    
-    	algo=tpe.suggest,
-    	max_evals=2,
-    	trials=trials)
-
-  print('Best: ', best)
+  for i in range(3):
+    run_trial()
   
 
 if __name__ == '__main__':
